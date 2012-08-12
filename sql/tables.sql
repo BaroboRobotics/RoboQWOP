9b9c41c6b4da4e79bb245d5b650fb737eb7b836c
Timothy Clemans <timothy.clemans@gmail.com>
2012-08-12 12:28:57 -0700

    added sql tables for basic teaching assignments

--- sql/tables.sql
+++ sql/tables.sql
@@ -2,11 +2,11 @@ USE barobo;
 
 /* Contains users waiting to control the Mobot */
 CREATE TABLE IF NOT EXISTS queue (
-	id INT NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY(id),
-	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	last_active TIMESTAMP NULL DEFAULT NULL,
-	user_id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_active TIMESTAMP NULL DEFAULT NULL,
+    user_id INT NOT NULL,
     robot_number INT NOT NULL DEFAULT 0,
     UNIQUE(user_id)
 ) ENGINE = InnoDB;
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS users (
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_seen TIMESTAMP NULL DEFAULT NULL,
     control_time INT DEFAULT 60, /* Time to control the mobot when added to the queue */
-	is_admin BOOLEAN NOT NULL DEFAULT 0,
+    is_admin BOOLEAN NOT NULL DEFAULT 0,
     UNIQUE (email)
 ) ENGINE = InnoDB;
 
@@ -32,10 +32,10 @@ CREATE TABLE IF NOT EXISTS robots (
     PRIMARY KEY(id),
     address CHAR(17),
     name VARCHAR(100),
-	color1_hex VARCHAR(6) DEFAULT '000000',
-	color2_hex VARCHAR(6) DEFAULT 'ffffff',
-	color1_name VARCHAR(10) DEFAULT 'Black',
-	color2_name VARCHAR(10) Default 'White',
+    color1_hex VARCHAR(6) DEFAULT '000000',
+    color2_hex VARCHAR(6) DEFAULT 'ffffff',
+    color1_name VARCHAR(10) DEFAULT 'Black',
+    color2_name VARCHAR(10) Default 'White',
     number INT NOT NULL,
     status INT NOT NULL DEFAULT 1,
     UNIQUE(number)
@@ -51,3 +51,33 @@ CREATE TABLE IF NOT EXISTS controllers (
     robot_number INT NOT NULL DEFAULT 0,
     unique(user_id)
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS courses (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    title varchar(100),
+    unique(title)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS assignments (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    course_id INT NOT NULL,    
+    number INT NOT NULL,
+    objective TEXT,
+    youtube_url VARCHAR(200),
+    instructions TEXT
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS users_last_completed_assignment_for_course (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    user_id INT NOT NULL,    
+    course_id INT NOT NULL,
+    last_completed_assignment_number INT NOT NULL   
+) ENGINE = InnoDB;
+
+INSERT INTO courses (title) values ('Robotics 101');
+INSERT INTO assignments (course_id, number, objective, instructions) values (1, 1, 'Move the robot to the wall.', 'Hitting W&O at the same time on your keyboard will move the robot one direction. Hitting O&P will move it in the other direction.');
+INSERT INTO assignments (course_id, number, objective, instructions) values (1, 2, 'Kick the ball to the wall', 'First get the robot next to the ball. Second spin the robot by pressing P&W (clockwise) or Q&O (counter-clockwise).');
+INSERT INTO assignments (course_id, number, objective, instructions) values (1, 3, 'Stand the robot up straight and knock it back down with legs straight out', "First get the robot rotated so that the joints move up and now not side to side. Second push both joints down rapidly. Depending on the robot's orientation, you'll need to press U&R down or I&E down.");
