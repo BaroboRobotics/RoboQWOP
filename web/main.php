3773a146ab2e756e824085235f1e848155b21f4a
Timothy Clemans <timothy.clemans@gmail.com>
2012-08-13 06:06:11 -0700

    wrote inline code comment about bug

--- web/main.php
+++ web/main.php
@@ -476,6 +476,7 @@ if (!isset( $_SESSION['user_id'] )) {
 			    showAssignment();
 				$("#completed_assignment").click(function() {
 				    $.post('completed_assignment.php', 'user_id='+current_user_id+'&assignment_number='+$('#assignment_number').text());
+					// not sure if processing time issue but you have to hit the completed assignment button twice before the next assignment appears
 					showAssignment();
 				});
                 $( "#control-tabs" ).tabs();
