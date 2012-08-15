703a64a1e73582fdf04d383291e505ed9df5a43c
Timothy Clemans <timothy.clemans@gmail.com>
2012-08-15 01:44:54 -0700

    add files

---
+++ web/exit_queue.php
@@ -0,0 +1,19 @@
+<?php
+include 'config.php';
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: index.php');
+    exit();
+}
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (mysqli_connect_errno()) {
+    header('Location: index.php');
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+$mysqli->query("DELETE FROM controllers WHERE user_id = " . $user_id); // remove user from controllers
+$mysqli->query("DELETE FROM queue WHERE user_id = " . $user_id); // remove user from queue
+$mysqli->close();
+header('Location: index.php');
+?>
