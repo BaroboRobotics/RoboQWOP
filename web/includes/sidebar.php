9d2980f586beb49eaa5b3fedf32d3a4a584de40a
Timothy Clemans <timothy.clemans@gmail.com>
2012-08-20 03:26:50 -0700

    per http://www.media-studies.ca/articles/influence.htm put more expensive item on top of ad

--- web/includes/sidebar.php
+++ web/includes/sidebar.php
@@ -6,13 +6,7 @@
 		<img src="img/promos/sidebar/top_hover.jpg" /><img src="img/promos/sidebar/bottom_hover.jpg" /></div>
 	    <div id="promo" class="rounded_corners">
 			<iframe src="http://www.youtube.com/embed/videoseries?list=PL1D8ED3B2BA1B3726" width="198" height="111" frameborder="0" allowfullscreen></iframe>
-			<a href="http://store.barobo.com/mobot/mobot.html" id="toppromo" class="promolink" style="margin-top:-5px;"><span>Buy a Mobot $269.95</span></a>
-			<div id="slideshow1" class="slideshow">
-    <img src="img/mobot.jpg" alt="A Mobot" class="active" />
-	<img src="img/four_mobots.jpg" alt="Four Mobots" />
-    <img src="img/smooth_mobot.jpg" alt="Smooth Mobot" />
-</div>
-			<a href="http://store.barobo.com/mobot/gorilla-bundle.html" id="bottompromo" class="promolink rounder_corners_bottom"><span>Buy three Mobots with connectors $827.75</span></a>
+			<a href="http://store.barobo.com/mobot/gorilla-bundle.html" style="margin-top:-5px;" id="bottompromo" class="promolink rounder_corners_bottom"><span>Buy three Mobots with connectors $827.75</span></a>
 		<div id="slideshow2" class="slideshow">
     <img src="img/big_mobot.jpg" alt="Big Mobot" class="active" />
 	<img src="img/rescue_mobot.jpg" alt="Rescue Mobots" />
@@ -21,6 +15,13 @@
 	<img src="img/triangle_mobot.jpg" alt="Triangle Mobot" />
 	<img src="img/mobot_standing_head_to_side.jpg" alt="Standing Head to Side" />
 </div>
+<a href="http://store.barobo.com/mobot/mobot.html" id="toppromo" class="promolink" style="margin-top:-5px;"><span>Buy a Mobot $269.95</span></a>
+			<div id="slideshow1" class="slideshow">
+    <img src="img/mobot.jpg" alt="A Mobot" class="active" />
+	<img src="img/four_mobots.jpg" alt="Four Mobots" />
+    <img src="img/smooth_mobot.jpg" alt="Smooth Mobot" />
+</div>
+			
 		</div>
 	    
 	</div>
