d21c36ff07e2c490c3f11863289ac9b0a03abd18
Timothy Clemans <timothy.clemans@gmail.com>
2012-08-21 04:55:32 -0700

    created about page

--- web/includes/navbar_general.php
+++ web/includes/navbar_general.php
@@ -1,4 +1,9 @@
-		    <?php if ($page == 'help') : ?>
+		    <?php if ($page == 'about') : ?>
+			    <strong>About</strong> | <a href="authenticate.php">Control a Mobot</a> | <a href="index.php">Watch queue</a> | 
+		    <?php else: ?>
+		        <a href="about.php">About RoboQWOP</a> | 
+			<?php endif; ?>
+			<?php if ($page == 'help') : ?>
 			    <strong>Help</strong> | <a href="authenticate.php">Control a Mobot</a> | <a href="index.php">Watch queue</a> | 
 		    <?php else: ?>
 		        <a href="help.php">Help</a> | 
